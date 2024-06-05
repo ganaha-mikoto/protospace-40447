f41597d52ee5384a20d02cc847bb5c60fa32ba38
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-06 00:38:50 +0900

    投稿機能を実装
     ヘッダーの「New Proto」ボタンから、新規投稿ページに遷移するようにパスを設定した（rails routesを用いて確認する）
     newアクションにインスタンス変数@prototypeを定義し、Prototypeモデルの新規オブジェクトを代入した
     new.html.erbから部分テンプレートである、_form.html.erbを呼び出す記述をした
     _form.html.erbのform_withのモデル名を正しいものに修正した
     _form.html.erbで「:hoge」と記載されている部分を、正しいものに修正した
     prototypesコントローラーのprivateメソッドにストロングパラメーターをセットし、特定の値のみを受け付けるようにした。且つ、user_idもmergeした
     createアクションにデータ保存のための記述をし、保存されたときはルートパスに戻るような記述をした
     createアクションに、データが保存されなかったときは新規投稿ページへ戻るようrenderを用いて記述した
     バリデーションによって保存ができず投稿ページへ戻ってきた場合でも、入力済みの項目（画像以外）は消えないことを確認した
     サーバーを再起動し、ブラウザで正しく動くか確認した
     Sequel Pro（Windowsの方はDBeaver）を確認して、正しく保存ができているか確認した
     validatesの誤字修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,20 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :concept, :catch_copy).merge(user_id: current_user.id)
   end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,9 +3,9 @@ has_many :comments
 belongs_to :user
 has_one_attached :image
 
-varidates :title, presence: true
-varidates :catch_copy, presence: true
-varidates :concept, presence: true
-varidates :image, presence: true
+validates :title, presence: true
+validates :catch_copy, presence: true
+validates :concept, presence: true
+validates :image, presence: true
 
 end
