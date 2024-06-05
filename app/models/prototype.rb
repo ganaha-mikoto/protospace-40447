2a1b6039140ed27e834152574fc0d0d0470d50cb
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-05 23:33:09 +0900

    バリデーションを設定
    Prototypeモデルに、プロトタイプの名称、キャッチコピー、コンセプト、画像に関するバリデーションを記述した

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,10 @@ class Prototype < ApplicationRecord
 has_many :comments
 belongs_to :user
 has_one_attached :image
+
+varidates :title, presence: true
+varidates :catch_copy, presence: true
+varidates :concept, presence: true
+varidates :image, presence: true
+
 end
