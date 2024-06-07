37410ab83a5941219b7aab8c84f5f84f522cac21
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-08 00:00:29 +0900

    プロトタイプ削除機能・編集機能を実装
    prototypesコントローラーにeditアクションとupdateアクションを設定した（まだアクション内の処理は書かない）
     resourcesを用いて、上記で設定したeditアクションとupdateアクションに対するルーティングをroutes.rbに記述した
     rails routesを実行して、ルーティングが正しく設定できていることを確かめた
      prototypesコントローラーにdestroyアクションを設定した（まだアクション内の処理は書かない）
     resourcesを用いて、上記で設定したdestroyアクションに対するルーティングをroutes.rbに記述した
     show.html.erbの「削除する」ボタンから、先ほど作成したルーティングが呼び込まれるようにした（パスはrails routesを用いて確認する。HTTPメソッドに注意する）
     destroyアクションに、プロトタイプを削除し、トップページに戻るような記述をした
     サーバーを再起動し、ブラウザで正しく動くか確認した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,7 +20,25 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
-  
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    if prototype.save
+      redirect_to root_path
+    else
+    redirect_to edit_prototype_path(prototype)
+    end
+  end
+
+def destroy
+  prototype = Prototype.find(params[:id])
+  prototype.destroy
+  redirect_to root_path
+end
 
   private
   def prototype_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes
 
   root "prototypes#index"
 end
