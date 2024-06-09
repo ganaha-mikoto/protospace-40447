bb6b3054bf7edfd4c9d804005918ee06ae580c6d
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-09 22:19:20 +0900

    コメント投稿機能準備
    Commentモデルおよびテーブルを作成・コントローラーを作成し、アクションとルーティングを設定/バリデーションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments, only: :create
   devise_for :users
   resources :prototypes
 
+  # Defines the root path route ("/")
   root "prototypes#index"
 end
 
