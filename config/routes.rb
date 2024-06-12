e986d40282dfdaf39d1f3371e2b12d80b266bad8
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-12 15:26:49 +0900

    ユーザー詳細ページでユーザーの情報が表示される
    usersコントローラーのshowアクションにインスタンス変数@userを定義した。且つ、Pathパラメータで送信されるID値で、Userモデルの特定のオブジェクトを取得するように記述し、それを@userに代入した
     ユーザー詳細ページで、ユーザーの各情報が正しく表示できるように修正した
     ユーザー詳細ページで、そのユーザーが投稿したプロトタイプが表示されるように、_prototype.html.erbの部分
     テンプレートを呼び出した（collectionオプションを使用する）
     エラーの解決・コードのコピー

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments, only: :create
   devise_for :users
-  resources :prototypes
-
-  # Defines the root path route ("/")
-  root "prototypes#index"
+  root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create 
+  end
+  resources :users, only: :show
 end
-
